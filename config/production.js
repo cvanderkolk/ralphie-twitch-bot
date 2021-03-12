5aee2054901b6bccd3ae50bf32cf6c94beb21fb0
Chris VanderKolk <christopher.n.vanderkolk@gmail.com>
2021-03-11 20:00:26 -0600

    Config with env vars and empty token json

---
+++ config/production.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "Twitch": {
+        clientId: process.env.TWITCH_CLIENT_ID,
+        clientSecret: process.env.TWITCH_CLIENT_SECRET,
+        refreshToken: process.env.TWITCH_REFRESH_TOKEN
+    },
+    "Streamlabs": {
+        accessToken: process.env.STREAMLABS_ACCESS_TOKEN
+    }
+}
