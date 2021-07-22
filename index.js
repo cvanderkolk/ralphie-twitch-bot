19435fa7c936da791ae1a54365cf4b47b7bcbd3c
Chris VanderKolk <christopher.n.vanderkolk@gmail.com>
2021-07-21 19:35:04 -0500

    Change sound source to self hosted clips

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const { ChatClient } = require('twitch-chat-client');
 const FormData = require('form-data');
 const { PubSubClient } = require('twitch-pubsub-client');
 
+const baseUrl = 'https://ralphie-twitch-bot.herokuapp.com';
+
 function getRandomItem(set) {
     const items = Array.from(set);
     return items[Math.floor(Math.random() * items.length)];
@@ -20,44 +22,44 @@ function getRandomItem(set) {
 
 const soundClipMap = {
     'Get Her Off Of Me': {
-        soundUrl: 'https://gdurl.com/XMDO/download',
+        soundUrl: 'get her off of me.mp3',
         imageUrl: 'https://i.pinimg.com/originals/77/c0/02/77c0028b38b7aa20391672260371d912.gif',
     },
     'Gagatrondra': {
-        soundUrl: 'https://gdurl.com/96za/download',
+        soundUrl: 'gagatrondra.mp3',
         imageUrl: 'https://media1.giphy.com/media/VFy9mDuUOTTQfWw39T/giphy.gif',
     },
     'Talented Brilliant': {
-        soundUrl: 'https://gdurl.com/Q7ZC/download',
+        soundUrl: 'talented brilliant.mp3',
         imageUrl: 'https://i.gifer.com/1YZx.gif',
     },
     'How Dare You': {
-        soundUrl: 'https://gdurl.com/senT/download',
+        soundUrl: 'how dare you.mp3',
         imageUrl: 'https://i.imgur.com/r75UZGW.gif?noredirect',
     },
     "I Can't Believe You've Done This": {
-        soundUrl: 'https://gdurl.com/BWJF/download',
+        soundUrl: "can't believe.mp3",
         imageUrl: 'https://thumbs.gfycat.com/HarmoniousEachEgg-small.gif',
     },
     'Not Today Satan': {
-        soundUrl: 'https://gdurl.com/DTKd/download',
+        soundUrl: 'not today satan.mp3',
         imageUrl: 'https://images.squarespace-cdn.com/content/v1/5e3ae820597c7c5ee13d77bf/1586334315884-80Z0TKTB6OPSRS7AGS6T/ke17ZwdGBToddI8pDm48kAI4xq1X9O62QpEWWz26IsVZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVF79dn-GXmv6G1Rqpzg9hme7yDMTHYtmNQQDOJBMZyks91lH3P2bFZvTItROhWrBJ0/not-today-satan.gif',
     },
     'Daddy Chill': {
-        soundUrl: 'https://gdurl.com/r6RI/download',
+        soundUrl: 'daddy chill.mp3',
         imageUrl: 'https://thumbs.gfycat.com/DampSimilarFrog-size_restricted.gif',
     },
     'Bastard People': {
         imageUrl: 'https://i.makeagif.com/media/9-13-2017/wH15a2.gif',
-        soundUrl: 'https://gdurl.com/23Cq/download',
+        soundUrl: 'corky.mp3',
     },
     'Hey Queen': {
         imageUrl: 'https://media.tenor.com/images/f5bd171d6fba0fe1f7cba84f13b2dd52/tenor.gif',
-        soundUrl: 'https://gdurl.com/Bw15/download',
+        soundUrl: 'hey queen.mp3',
     },
     'Wha Happen': {
         imageUrl: 'https://media4.giphy.com/media/l2JIk0sWj9sUvbvCU/giphy.gif',
-        soundUrl: 'https://gdurl.com/Xc5g/download',
+        soundUrl: 'wha happen.mp3',
     },
 };
 
@@ -69,7 +71,7 @@ async function sendStreamlabsAlert(imageUrl, soundUrl, message) {
     formdata.append("image_href", imageUrl);
     formdata.append("message", message);
     formdata.append("duration", "5000");
-    formdata.append("sound_href", soundUrl);
+    formdata.append("sound_href", `${baseUrl}/${soundUrl}`);
 
     const response = await fetch(url, { method: 'POST', body: formdata });
     console.log(await response.json());
